97b8b10b845512c960f904e86b306a350ccafc39
ristim <32619521+ristim@users.noreply.github.com>
2021-05-22 20:14:51 +0200

    odd-types |added javam builder class. For fun and entertainment. Not the way to go, but I don't see an alternative without using reflection for now

--- typescript/src/string-to-object/stringConverter.ts
+++ typescript/src/string-to-object/stringConverter.ts
@@ -3,6 +3,6 @@ import {StringToObject} from "./stringToObject";
  * Quick and dirty function to test the StringToObject type
  */
 export function replaceKeysInString<K extends string>(old: K, mapTo: StringToObject<K> ): string {
-    return Object.entries<string>(mapTo).reduce((text,kv) => text.replace(kv[0], kv[1]), old)
+    return Object.entries<string>(mapTo).reduce((text,kv) => text.replace(':'+kv[0], kv[1]), old)
 }
 
--- typescript/src/string-to-object/stringToObject.test.ts
+++ typescript/src/string-to-object/stringToObject.test.ts
@@ -5,6 +5,6 @@ test('Three objects in a string', () => {
     * Changing the keys in the map, will result in a TS2345
     */
    let result = replaceKeysInString("1:second/:third/4/:fifth/6",{second: "2", third: "3", fifth: "5"})
-   expect(result).toBe("1:2/:3/4/:5/6")
+   expect(result).toBe("12/3/4/5/6")
 });
 
